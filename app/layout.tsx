20854ff2b0c19086e2c95f1292b92ba54186f88a
Muzammal-Rafique <muzammalrafique26@gmail.com>
2024-03-06 07:22:39 +0500

    Fixed about me section

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "MUZAMMAL RAFIQUE",
   description: "Muzammal Rafique Offical Portfolio",
+  icons:{
+    icon : "./favicon.ico"
+  }
 };
 
 export default function RootLayout({
--- components/About.tsx
+++ components/About.tsx
@@ -14,59 +14,39 @@ const TabsComponent = () => {
         </span>
       </h1>
       <div className="flex flex-col-reverse lg:flex-row-reverse p-4 lg:p-16">
-        {/* Right Side - Tabs */}
-        <div className="w-full lg:w-1/2 pl-10">
-          {/* Muzammal Rafique Heading */}
-          <h2 className="text-3xl text-[#f43f5e] font-bold mb-4">
-            Muzammal Rafique
-          </h2>
+  <div className="w-full lg:w-1/2 pl-10">
+    <h2 className="text-3xl text-black font-bold mt-6 mb-4">
+      Muzammal Rafique
+    </h2>
+    <p className="text-2xl font-bold text-[#f43f5e] ">Background</p>
+    <p className="mb-6 mt-2 text-xl text-justify">
+    Recently graduated in Information Technology from Government College University Faisalabad, I am currently immersed in the world of web development, specializing in Next.js and React.js. This academic journey has equipped me with a solid foundation in IT principles and methodologies, providing me with the necessary tools to excel in the ever-evolving tech landscape.
+    </p>
+    <p className="text-2xl font-bold text-[#f43f5e] ">Contemporary</p>
+    <p className="mb-6 mt-2 text-xl text-justify">
+    My passion lies in pushing the boundaries of technology, and I am proficient in Next.js and React.js for dynamic web application development. I am also an adaptable learner, committed to staying updated with emerging technologies. Embracing the challenges of the present, I continuously seek opportunities to expand my knowledge and refine my skills in pursuit of excellence.
+    </p>
+    <p className="text-2xl font-bold text-[#f43f5e] ">Future Plan</p>
+    <p className="mb-6 text-xl mt-2 text-justify">
+    My future goal is to evolve into a skilled Blockchain and Web 3.0 developer, contributing to the decentralized and innovative future of the internet. I aspire to become a valuable asset in this rapidly evolving field, leveraging my problem-solving skills and collaborative nature to drive forward projects and initiatives. With a clear vision of where I want to be, I am dedicated to taking the necessary steps to achieve my aspirations and make a meaningful impact in the tech industry.
+    </p>
+    
+  </div>
 
-          {/* Summary */}
-          <p className="mb-4 text-xl text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-900">
-            Recently graduated in Information Technology from Government College
-            University Faisalabad, I am currently immersed in the world of web
-            development, specializing in Next.js and React.js. My passion lies
-            in pushing the boundaries of technology, and my future goal is to
-            evolve into a skilled Blockchain and Web 3.0 developer, contributing
-            to the decentralized and innovative future of the internet.
-          </p>
-          <p className="text-2xl font-bold text-[#f43f5e] ">Key Points</p>
-          <ul className="list-disc text-xl pl-4 mb-4 text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-900">
-            <li className="mb-2">
-              Proficient in Next.js and React.js for dynamic web application
-              development.
-            </li>
-            <li className="mb-2">
-              Aspiring Blockchain and Web 3.0 developer with a keen interest in
-              decentralized systems.
-            </li>
-            <li className="mb-2">
-              Problem-solving skills honed through academic projects and
-              practical applications.
-            </li>
-            <li className="mb-2">
-              Adaptable learner, committed to staying updated with emerging
-              technologies.
-            </li>
-            <li className="mb-2 ">
-              Collaborative team member, valuing communication and cooperation
-              in project development.
-            </li>
-          </ul>
-          {/* Tabs */}
-        </div>
+  <div className="lg:w-1/2 mb-8 lg:mb-0 ml-10">
+    <div style={{ width: '100%', height: '80%' }}>
+      <Image
+        src="/Github.png"
+        alt="Left Side Image"
+        layout="responsive"
+        width={500}
+        height={300}
+        className="rounded-lg shadow-lg"
+      />
+    </div>
+  </div>
+</div>
 
-        {/* Left Side - Image */}
-        <div className="lg:w-1/2 mb-8 lg:mb-0 ml-10 pl-20">
-          <Image
-            src="/github.png"
-            alt="Left Side Image"
-            width={500}
-            height={300}
-            className=" rounded-lg shadow-lg"
-          />
-        </div>
-      </div>
     </>
   );
 };
